e940e7e99118cbfd37d5e63f37460611fe7bcc22
Linda Cole <colelc@gmail.com>
2024-01-31 14:16:03 -0500

    commit

--- routes/acronyms.js
+++ routes/acronyms.js
@@ -1,19 +1,33 @@
 
 const express = require("express");
 const router = express.Router();
-const pg = require("pg");
 const db = require("../db/postgres");
 
 // Define route for get all acronyms
 // http://localhost:3050/acronyms
-router.get("/", (request, response) => {
-    response.send("This is the get all acronyms route");
-});
+// router.get("/", (request, response) => {
+//     response.send("This is the get all acronyms route");
+// });
 
 // Define route for get a specific acronym by id
-// http://localhost:3050/acronyms/101
-router.get("/101", (request, response) => {
-    response.send("This the the get for acronym id=101");
+// http://localhost:3050/acronyms/71
+router.get("/pg/:id", (request, response) => {
+    const id = request.params.id;
+    console.log(id);
+
+    db.client.connect(async (err) => {
+        if (err) {
+            console.log("We have an error", err);
+        } else {
+            const queryConfig = {
+                text: "SELECT * FROM fuqua_acronyms WHERE id = $1",
+                values: [id]
+            };
+            const result = await db.client.query(queryConfig);
+            console.log("result!", result.rows[0]);
+            response.send(result.rows);
+        }
+    });
 });
 
 // Define route to test postgres connectivity
